1f1e25504d41514fdc88d7e2b805b33a734126b2
Alek <alekmitrevski@hotmail.com>
2026-02-24 16:01:26 -0700

    cleave 1.0.31

---
+++ Formula/cleave.rb
@@ -0,0 +1,71 @@
+class Cleave < Formula
+  desc "AI-powered GitHub PR analysis - split large PRs into reviewable groups"
+  homepage "https://github.com/dnamaz/cleave"
+  version "1.0.31"
+  license "MIT"
+
+  on_macos do
+    url "https://github.com/dnamaz/cleave/releases/download/v#{version}/cleave-#{version}-macos-arm64.tar.gz"
+    sha256 "8746c33eefded445f6db7ccd91761ff4db98094665aa68c292140b4a0a1c5671"
+  end
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "https://github.com/dnamaz/cleave/releases/download/v#{version}/cleave-#{version}-linux-arm64.tar.gz"
+      sha256 "PLACEHOLDER"
+    else
+      url "https://github.com/dnamaz/cleave/releases/download/v#{version}/cleave-#{version}-linux-x86_64.tar.gz"
+      sha256 "PLACEHOLDER"
+    end
+  end
+
+  def install
+    bin.install "cleave"
+    libexec.install "cleave-bin"
+
+    inreplace bin/"cleave" do |s|
+      s.gsub! 'CLEAVE_BIN="$SCRIPT_DIR/cleave-bin"', "CLEAVE_BIN=\"#{libexec}/cleave-bin\""
+    end
+  end
+
+  def post_install
+    (var/"log").mkpath
+  end
+
+  service do
+    run [opt_bin/"cleave"]
+    keep_alive true
+    log_path var/"log/cleave.log"
+    error_log_path var/"log/cleave-error.log"
+    working_dir var/"cleave"
+  end
+
+  def caveats
+    <<~EOS
+      First-time setup:
+        cleave setup
+
+      You'll need your org's GitHub credentials
+      (ask your team lead or check 1Password).
+
+      Start the app:
+        cleave
+
+      Then open: http://localhost:9090
+
+      Other commands:
+        cleave status    # show configuration
+        cleave reset     # remove config and data
+
+      Run as background service:
+        brew services start cleave
+
+      Config: ~/.config/cleave/
+      Logs:   #{var}/log/cleave.log
+    EOS
+  end
+
+  test do
+    assert_match "Usage: cleave", shell_output("#{bin}/cleave help")
+  end
+end
